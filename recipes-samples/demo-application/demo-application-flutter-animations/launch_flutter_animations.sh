15363c13b4b6d980272e20b277906f39e90ad7f7
MEd-Douiri <mr.mouad.ed.douiri@gmail.com>
2022-08-04 14:06:59 +0200

    update installation path

--- recipes-samples/demo-application/demo-application-flutter-animations/launch_flutter_animations.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#start animations
-flutter-client -d -w 800 -h 399 -b /usr/share/flutter/animations/flutter_assets/
--- recipes-samples/demo-application/demo-application-flutter-gallery/launch_flutter_gallery.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#start gallery
-flutter-client -d -w 800 -h 399 -b /usr/share/flutter/gallery/flutter_assets/
