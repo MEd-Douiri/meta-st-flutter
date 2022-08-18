4ac5c9bd8d0020bd1f13b94b70016ba72cf6af9e
MEd-Douiri <mr.mouad.ed.douiri@gmail.com>
2022-08-18 11:27:11 +0200

    update display size

--- recipes-samples/demo-application/demo-application-flutter-gallery/launch_flutter_gallery.sh
+++ recipes-samples/demo-application/demo-application-flutter-gallery/launch_flutter_gallery.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #start gallery
-flutter-client -d -w 800 -h 399 -b /usr/share/flutter/gallery/flutter_assets/
+flutter-client -d -w 800 -h 480 -b /usr/share/flutter/gallery/flutter_assets/
