1f21b2c853f4ef5eea521dccf26d6dd20d97c81d
MEd-Douiri <mr.mouad.ed.douiri@gmail.com>
2022-08-18 11:22:03 +0200

    update display size

--- recipes-samples/demo-application/demo-application-flutter-homescreen/launch_flutter_homescreen.sh
+++ recipes-samples/demo-application/demo-application-flutter-homescreen/launch_flutter_homescreen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #start material 3 demo
-flutter-client -d -w 800 -h 399 -b /usr/share/flutter/flutter_homescreen/flutter_assets/
+flutter-client -d -w 800 -h 480 -b /usr/share/flutter/flutter_homescreen/flutter_assets/
--- recipes-samples/demo-application/demo-application-flutter-material-3/launch_flutter_material_3.sh
+++ recipes-samples/demo-application/demo-application-flutter-material-3/launch_flutter_material_3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #start homescreen
-flutter-client -d -w 800 -h 399 -b /usr/share/flutter/material_3_demo/flutter_assets/
+flutter-client -d -w 800 -h 480 -b /usr/share/flutter/material_3_demo/flutter_assets/
